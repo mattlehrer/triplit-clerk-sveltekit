c410adcc506a76fe0a9d969be255e1c942c7b4b4
Matt Lehrer <lehrerm@gmail.com>
2024-08-23 14:01:20 +0200

    init

---
+++ src/lib/client.ts
@@ -0,0 +1,27 @@
+import { TriplitClient } from '@triplit/client';
+import { schema } from '../../triplit/schema';
+import {
+  PUBLIC_TRIPLIT_SERVER_URL,
+  PUBLIC_TRIPLIT_TOKEN,
+} from '$env/static/public';
+import { browser } from '$app/environment';
+
+// The TriplitClient has 4 main options
+// - storage: The storage engine you want to use. This can be
+//   'memory' or 'indexeddb'.
+// - schema: The schema you defined for your app, which
+//   will be used to generate types for client methods
+//   and handle local database operations
+// - serverUrl: The URL of your Triplit server
+// - token: The token you got from the Triplit dashboard
+//
+// Without the serverUrl or token, the client will operate in
+// offline mode
+
+export const triplit = new TriplitClient({
+  storage: 'memory',
+  schema,
+  serverUrl: PUBLIC_TRIPLIT_SERVER_URL,
+  token: PUBLIC_TRIPLIT_TOKEN,
+  autoConnect: browser,
+});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { useQuery } from '@triplit/svelte';
+  import { triplit } from '$lib/client';
+  import GettingStarted from './getting-started.svelte';
+  import ConnectionStatus from './connection-status.svelte';
+  import Todo from './todo.svelte';
+
+  let text = $state('');
+  const todos = useQuery(
+    triplit,
+    triplit.query('todos').order('created_at', 'DESC'),
+  );
+  let todosArray = $derived(todos.results ? Array.from(todos.results) : []);
+</script>
+
+<div class="main-container">
+  <GettingStarted />
+  <div class="app-container">
+    <h1>Todos</h1>
+    <ConnectionStatus />
+    <form
+      onsubmit={async (e) => {
+        e.preventDefault();
+        await triplit.insert('todos', { text });
+        text = '';
+      }}
+    >
+      <input
+        type="text"
+        placeholder="What needs to be done?"
+        class="todo-input"
+        bind:value={text}
+      />
+      <button class="btn" type="submit" disabled={!text}> Add Todo </button>
+    </form>
+    {#if todos.fetching}
+      <p>Loading...</p>
+    {/if}
+    {#if todos.results}
+      <div class="todos-container">
+        {#each todosArray as [_key, todo]}
+          <Todo {todo} />
+        {/each}
+      </div>
+    {/if}
+  </div>
+</div>
