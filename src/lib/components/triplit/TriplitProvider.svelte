711f1734ff5a03f9b94b47ea8bf892ef1ef1eb93
Matt Lehrer <lehrerm@gmail.com>
2024-08-23 15:14:05 +0200

    add triplit context

---
+++ src/lib/components/triplit/TriplitProvider.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { setTriplitContext } from './context';
+	import { useClerkContext } from 'svelte-clerk';
+	import { offlineDb, onlineDb } from '$lib/client';
+
+	const {
+		token,
+		children,
+	}: {
+		token: string | null;
+		children: Snippet;
+	} = $props();
+
+	const clerk = useClerkContext();
+
+	// use token from SSR if available instead of waiting for it to load client side
+	let currentTriplitToken: string | undefined | null = token;
+	if (currentTriplitToken) {
+		onlineDb.updateToken(currentTriplitToken);
+	}
+
+	async function updateToken(session: typeof clerk.session) {
+		const currentSessionToken = session?.lastActiveToken?.getRawString();
+		if (!session) {
+			currentTriplitToken = null;
+			onlineDb.disconnect();
+		} else if (currentTriplitToken !== currentSessionToken) {
+			const lastActiveTokenExp = session?.lastActiveToken?.jwt?.claims.exp;
+
+			if (!lastActiveTokenExp || lastActiveTokenExp * 1000 < new Date().getTime()) {
+				console.log('Token expired, getting new token');
+				currentTriplitToken = await session?.getToken();
+			} else {
+				currentTriplitToken = currentSessionToken;
+			}
+
+			if (currentTriplitToken) {
+				// console.log('updating token to', currentTriplitToken);
+				onlineDb.updateToken(currentTriplitToken);
+			} else {
+				onlineDb.disconnect();
+			}
+		} else {
+			console.log('Token is the same');
+		}
+	}
+
+	// when session changes, update Triplit token
+	$effect(() => {
+		updateToken(clerk.session);
+	});
+
+	setTriplitContext({
+		get onlineDb() {
+			return onlineDb;
+		},
+		get offlineDb() {
+			return offlineDb;
+		},
+	});
+</script>
+
+{@render children()}
